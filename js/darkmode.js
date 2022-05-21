401fad69a50e36a218566d771d37ebd0a3d581c3
Dave Lavilla <davelavilla@gmail.com>
2022-05-21 08:47:51 +0800

    added thankyou.html

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,10 +1,18 @@
 function darkMode() {
   firstViewDarkMode();
+  thankYouDarkMode();
 }
 
 function firstViewDarkMode() {
   let element = document.querySelector(".first-view");
   if (element) {
     element.classList.toggle("first-view-dark");
+  }
+}
+
+function thankYouDarkMode() {
+  let element = document.querySelector(".thank-you-page");
+  if (element) {
+    element.classList.toggle("thank-you-page-dark");
   }
 }
